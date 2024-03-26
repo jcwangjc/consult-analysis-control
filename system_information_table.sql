b87e59b7ea766d2f98f793534bb160a2ea1d3f36
laoa <laoa@markcoin.net>
2024-03-26 17:46:37 +0700

    test

---
+++ system_information_table.sql
@@ -0,0 +1,16 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 80017
+ Source Host           : localhost:3306
+ Source Schema         : xxl_job
+
+ Target Server Type    : MySQL
+ Target Server Version : 80017
+ File Encoding         : 65001
+
+ Date: 26/03/2024 17:35:01
+*/
+
